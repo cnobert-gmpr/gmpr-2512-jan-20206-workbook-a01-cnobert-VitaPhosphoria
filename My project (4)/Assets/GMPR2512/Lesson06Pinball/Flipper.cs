3d87be8c31a83cef7ad8780eb3ffa2573bc08d65
VitaPhosphoria <gildeddreamers@gmail.com>
2026-02-11 11:46:32 -0700

    Updated Flipper.cs

--- My project (4)/Assets/GMPR2512/Lesson06Pinball/Flipper.cs
+++ My project (4)/Assets/GMPR2512/Lesson06Pinball/Flipper.cs
@@ -5,14 +5,19 @@ namespace GMPR2512.Lesson06Pinball
     public class Flipper : MonoBehaviour
     {
         private HingeJoint2D _joint2D;
-
+        [SerializeField] private bool _rightFlipper;
         void Awake()
         {
             _joint2D = GetComponent<HingeJoint2D>();
+            
         }
         void Update()
         {
-        
+            if(_rightFlipper)
+                _joint2D.useMotor = Input.GetKey(KeyCode.RightShift);
+            else
+                _joint2D.useMotor = Input.GetKey(KeyCode.LeftShift);
+            
         }
     }
 }
